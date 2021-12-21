e9af18422b866ceddaf76685105b0c5b99e74348
Sergi Bofu <sergibofu@gmail.com>
2021-12-21 19:55:25 +0100

    La api ahora retorna la lista de categorias del post junto con el post cuando se piden los detalles de un post

--- blog_api/posts/views.py
+++ blog_api/posts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+
+#Posts model and serializer 
 from posts.models import Post
 from posts.serializers import PostSerializer
+
+#Categories and PostCategory models and serializers
+from categories.models import Category, PostCategory
+from categories.serializers import CategorySerializer , PostCategorySerializer  
+
+
+
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
@@ -11,3 +21,29 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get(self, request, pk):
+        try:
+            #Primero recuperamos nuestro modelo
+            post = Post.objects.get(pk=pk)
+            postSerializer = PostSerializer(post, many=False) 
+
+            #Con la clave primaria de nuestro modelo, recuperamos todas las relaciiones post-categoria
+            #asociadas a nuestro post
+            listOfCategoriesPost = PostCategory.objects.filter(post_id = pk)
+            listOfCategoriesPostSerializer = PostCategorySerializer(listOfCategoriesPost, many=True)
+
+            listOfcategories = []
+            for category in listOfCategoriesPostSerializer.data:
+                listOfcategories.append(category['category'])
+            
+            
+            #ahora recuperamos los nombres de nuestras categorias en la bbdd
+            categories = Category.objects.filter(pk__in=listOfcategories)
+            categoriesSerializer = CategorySerializer(categories, many=True)
+
+            return Response({'post': postSerializer.data, 'categories': categoriesSerializer.data})
+
+        except Category.DoesNotExist:
+            return Response({'error': 'error retriving database'})
+    
